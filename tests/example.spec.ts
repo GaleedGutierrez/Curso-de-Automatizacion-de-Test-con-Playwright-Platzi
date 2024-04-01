115ca4173bfadd3846ce56e5ff851a1aaefc3a62
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-04-01 10:48:06 -0300

    test: :test_tube: Clase N°3
    Cualquiera puede escribir tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,9 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-	await page.goto('https://playwright.dev/');
-
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-	await page.goto('https://playwright.dev/');
-
-	// Click the get started link.
-	await page.getByRole('link', { name: 'Get started' }).click();
-
-	// Expects page to have a heading with the name of Installation.
-	await expect(
-		page.getByRole('heading', { name: 'Installation' }),
-	).toBeVisible();
+test('From codegen', async ({ page }) => {
+	await page.goto('https://codepen.io/');
+	await page.getByRole('link', { name: 'Search Pens' }).click();
+	await page.locator('[data-test-id="search-input"]').click();
+	await page.locator('[data-test-id="search-input"]').fill('duck');
+	await page.locator('[data-test-id="search-input"]').press('Enter');
 });
