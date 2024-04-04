3c017c2d05fa21c336bd016a5a5f16e6f19ea4be
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-04-04 02:03:58 -0300

    test: :test_tube: Skip test

--- tests/firstChallenge.spec.ts
+++ tests/firstChallenge.spec.ts
@@ -3,21 +3,13 @@ import { expect, test } from '@playwright/test';
 const URL_BASE = new URL('https://automationexercise.com');
 const URL_PRODUCT_DETAILS_FIRST = new URL('/product_details/1', URL_BASE);
 
-test.describe('Fist challenge', () => {
+test.describe.skip('Fist challenge', () => {
 	test('Add product to cart', async ({ page }) => {
 		const SCROLL_DOWN = 640;
 
 		await page.goto(URL_BASE.href);
 		await page.mouse.wheel(0, SCROLL_DOWN);
 		await page.getByRole('link', { name: 'View Product' }).first().click();
-
-		const URL = page.url();
-		const URL_GOOGLE = 'https://automationexercise.com/#google_vignette';
-
-		if (URL === URL_GOOGLE) {
-			await page.getByRole('button', { name: 'Continue' }).click();
-		}
-
 		await expect(page).toHaveURL(URL_PRODUCT_DETAILS_FIRST.href);
 
 		const QUANTITY_INPUT = page.locator('#quantity');
